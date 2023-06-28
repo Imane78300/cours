33bcf686b63dc7e0b6abb562f25a50e9bdb29f3a
Imane <amouna.narimi@gmail.com>
2023-06-28 16:56:37 +0200

    imane

---
+++ GRID/JS/activ.js
@@ -0,0 +1,7 @@
+// navigation avec lien savtif sur la page en cours
+
+// 1-ajouter l 4id #active _nav sur la balise <nav>
+
+// -2 vérifier que la classe .active est bien dans le fishier css
+
+// -3 crée la fonction setActive () pour pour ajouter la classe .activ sur le lien URL en cours
